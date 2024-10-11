3e6224e527b61f5d237a4a9604e5edc82ee964d2
hykw-r <reo.hayakawa@link-u.co.jp>
2024-10-11 22:16:17 +0900

    Add a category view

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by hayakawa on 2024/10/11.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
