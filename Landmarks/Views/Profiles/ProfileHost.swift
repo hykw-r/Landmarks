a627eda3b2997b652c7260dc4853e9a466cec8c3
hykw-r <reo.hayakawa@link-u.co.jp>
2024-10-13 02:41:00 +0900

    Display a user profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by hayakawa on 2024/10/13.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
